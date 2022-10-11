50935419eda94699c54da5ed7a50144272409f5a
SVETIK-LION <svetlana.lvovskaya@gmail.com>
2022-10-11 08:35:32 +0000

    lesson_3 Template's context

--- mainapp/views.py
+++ mainapp/views.py
@@ -13,7 +13,9 @@ class NewsPageView(TemplateView):
         context = super().get_context_data(**kwargs)
         # Create your own data
         context["news_title"] = "Громкий новостной заголовок"
-        context["news_preview"] = "Предварительное описание, которое заинтересует каждого"
+        context[
+            "news_preview"
+        ] = "Предварительное описание, которое заинтересует каждого"
         return context
 
 
